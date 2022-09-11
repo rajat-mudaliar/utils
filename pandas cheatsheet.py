5003cb71f19ceba28183b8ac85a895b50fc4597a
Rajat Mudaliar <59509695+rajat-mudaliar@users.noreply.github.com>
2022-09-11 14:38:39 +0530

    Update pandas cheatsheet.py

--- pandas cheatsheet.py
+++ pandas cheatsheet.py
@@ -2,3 +2,11 @@ import pandas as pd
 
 #Multiple lists to dataframe-- text_content,text_label,text_pred,image_path,image_label,image_pred are lists
 final_df=pd.DataFrame({'text_content':text_content,'text_label':text_label,'text_pred':text_pred,'image_path':image_path,'image_label':image_labels,'image_pred':image_pred})
+
+#create new column based on condition of two other columns
+def new_fn(df):
+    if df.column1=="abc" or df.column2=="xyz":
+        return "yes"
+    else:
+        return "no"
+df['new_column'] = df.apply(new_fn, axis=1)
