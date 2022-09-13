0a2e35bc27bf1ee9b6ac88fb9e2864bd835ee653
Rajat Mudaliar <59509695+rajat-mudaliar@users.noreply.github.com>
2022-09-13 12:15:38 +0530

    Update passing_argument_to_python_file.py

--- passing_argument_to_python_file.py
+++ passing_argument_to_python_file.py
@@ -1,5 +1,7 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--foo', help='foo help')
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-label_path", "--label_path")
 args = parser.parse_args()
-print(args)
+label_path=args.label_path
