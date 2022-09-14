6324ea8b51100af492fc9ab368b63a663e5d6b4d
Rajat Mudaliar <59509695+rajat-mudaliar@users.noreply.github.com>
2022-09-14 22:44:37 +0530

    Update file_operations.py

--- file_operations.py
+++ file_operations.py
@@ -4,4 +4,4 @@ with open(filename) as file:
     lines = [line.rstrip() for line in lines]
 #get list of all files recursively
 import os
-allfiles = [os.path.join(root,f) for root,dirs,files in os.walk('path to folder') for f in files]
+allfiles = [os.path.join(root,f) for root,dirs,files in os.walk('path to folder') for f in files] #https://stackoverflow.com/a/13051819
