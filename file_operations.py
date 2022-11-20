21f02a018f54c4f1a55e56c7833e1ac2e3be91c6
Rajat Mudaliar <59509695+rajat-mudaliar@users.noreply.github.com>
2022-11-20 10:05:45 +0530

    Update file_operations.py

--- file_operations.py
+++ file_operations.py
@@ -5,3 +5,17 @@ with open(filename) as file:
 #get list of all files recursively
 import os
 allfiles = [os.path.join(root,f) for root,dirs,files in os.walk('path to folder') for f in files] #https://stackoverflow.com/a/13051819
+
+#change all yolo labels class number
+headlabels=os.listdir('label folderpath')
+for i in headlabels:
+    print(i)
+    f=open('label folderpath'+i,'r')
+    lb=f.readlines()
+    # print(lb)
+    f.close()
+    newlb=['1'+j[1:] for j in lb ]
+    # print(newlb)
+    write=open('label folderpath'+i,'w')
+    write.writelines(newlb)
+    write.close()
